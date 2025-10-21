f3164fa80865ae84e3970f229690f2683da0cce0
SauravNaruka <saurav.singh.n@gmail.com>
2025-10-21 17:34:20 +0530

    Add stop word remove logic

---
+++ internal/search/client.go
@@ -0,0 +1,53 @@
+package search
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Client struct {
+	movies      []Movie
+	stopWordMap map[string]string
+}
+
+type Movie struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type MovieData struct {
+	Movies []Movie `json:"movies"`
+}
+
+func NewClient(dataFilePath string, stopWordsFilePath string) (*Client, error) {
+	movieDataRaw, err := os.ReadFile(dataFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	var movieData MovieData
+	if err := json.Unmarshal(movieDataRaw, &movieData); err != nil {
+		return nil, fmt.Errorf("error parsing JSON: %w", err)
+	}
+
+	stopWordsData, err := os.ReadFile(stopWordsFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading stop words file: %w", err)
+	}
+
+	stopWords := strings.Fields(string(stopWordsData))
+	stopWordMap := make(map[string]string, len(stopWords))
+	for _, w := range stopWords {
+		stopWordMap[w] = ""
+	}
+
+	c := Client{
+		movies:      movieData.Movies,
+		stopWordMap: stopWordMap,
+	}
+
+	return &c, nil
+}
